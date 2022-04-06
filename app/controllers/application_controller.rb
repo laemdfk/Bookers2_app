c868daa5a837104759c548fa4f805152d93fa0dc
EC2 Default User <ec2-user@ip-172-31-2-155.ap-northeast-1.compute.internal>
2022-04-06 14:21:40 +0000

    [Add]deviseログイン、サインアップ、記述変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  #現在サインインしてるユーザーのshowページに飛ばす
+
+  def after_sign_in_path_for(resource)
+   user_path(current_user.id)
+  end
+
+
+  def after_sign_out_path_for(resource)
+   root_path
+  end
+
+  protected
+
+ #config側で「名前で認証」設定しているため、こちらでemailを許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
