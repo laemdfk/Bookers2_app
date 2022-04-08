514db5bb6a56df45e5b952df0edcde699c0de466
EC2 Default User <ec2-user@ip-172-31-2-155.ap-northeast-1.compute.internal>
2022-04-08 17:29:07 +0000

    　[Add]ビュー変更、コントローラー書き換え

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,61 +4,76 @@ class BooksController < ApplicationController
 protect_from_forgery
 
 
-before_action :corrent_user, only: [:edit, :destroy]
+ before_action :authenticate_user!
+     before_action :ensure_current_user, {only: [:edit,:update,:destroy]}
+     #(ログインユーザー以外の人が情報を遷移しようとした時に制限をかける)
 
- def index
-  @books = Book.all
-  @book = Book.new
- end
+	def create
+        @user = current_user
+		@book = Book.new(book_params)
+        @book.user_id = (current_user.id)
+	    if @book.save
+		redirect_to  book_path(@book.id)
+        # redirect_to "/books/#{@book.id}"
 
- def create
-   @book = Book.new(book_params)
-    #@book.user_id = current_user.id
-  if @book.save
-   redirect_to books_path(@book.id)
+        else
+        @books = Book.all
+        flash[:notice] = ' errors prohibited this obj from being saved:'
+        render "index"
+        end
+	end
 
-  else
-   @books = Book.all
-    render  'index'
-  end
- end
-
- def show
-  @book = Book.find(params[:id])
- end
-
- def edit
- @book = Book.find(params[:id])
- end
-
- def updete
-   @book = Book.find(params[:id])
-     if @book.update(book_params)
-      flash[:notice] = "Book was successfully update."
-     redirect_to book_path(book.id)
-     else
-     @books = Book.all
-      render 'edit'
+    def show
+        @user = current_user
+    	@book = Book.find(params[:id])
+    	@book_new = Book.new
     end
- end
 
- def destroy
-     @book = Book.find(params[:id])
-     @book.destroy
-     flash[:notice]="Book was successfully destroyed."
-     redirect_to books_path
- end
+    def index
+        @user = current_user
+        @books = Book.all
+        @book = Book.new
+    end
+
+    def edit
+    	@book = Book.find(params[:id])
+    end
 
 
-# 以下、ストロングパラメータ/必ずラストのエンド前に。
-private
- def book_params
-  params.require(:book).permit(:title, :body)
- end
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+        flash[:notice] = "You have creatad book successfully."
+        redirect_to  book_path(@book.id)
 
-  def corrent_user
- @book = Book.find(params[:id])
- @user = @book_user
-  redirect_to(books_path) unless @user==current_user
+        else
+        @books = Book.all
+         flash[:notice]= ' errors prohibited this obj from being saved:'
+        render "edit"
+        end
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to "/books"
+     end
+
+	private
+
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+
+     def user_params
+        params.require(:user).permit(:name,:profile_image,:introduction)
+   end
+
+     def  ensure_current_user
+      @book = Book.find(params[:id])
+     if @book.user_id != current_user.id
+        redirect_to books_path
+     end
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,45 @@
 class UsersController < ApplicationController
 
- def index
-  @books = Book.all
-  @book = Book.new
- end
-
- def create
-   @book = Book.new(book_params)
-    @book.user_id = current_user.id
-  if @book.save
-   redirect_to book_path(@book.id)
-
-  else
-   @books = Book.all
-    render  'index'
-  end
- end
-
- def show
-  @book = Book.find(params[:id])
- end
-
- def edit
- @book = Book.find(params[:id])
- end
-
- def updete
-   @book = Book.find(params[:id])
-     if @book.update(book_params)
-      flash[:notice] = "Book was successfully update."
-     redirect_to book_path(book.id)
-     else
-     @books = Book.all
-      render 'edit'
+lass UsersController < ApplicationController
+  before_action :correct_user, only: [:edit]
+  def correct_user
+    user = User.find(params[:id])
+    if current_user.id != user.id
+      redirect_to user_path(current_user)
     end
- end
+  end
 
- def destroy
-     @book = Book.find(params[:id])
-     @book.destroy
-     flash[:notice]="Book was successfully destroyed."
-     redirect_to books_path
- end
+  def home
+  end
 
+  def index
+    @users = User.all
+    @book = Book.new
+  end
 
+  def show
+    @user = User.find(params[:id])
+    @books = Book.where(user_id: params[:id])
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User info was successfully updated"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
 # 以下、ストロングパラメータ/必ずラストのエンド前に。
-private
- def book_params
-  params.require(:book).permit(:title, :body)
- end
-
-  def corrent_user
- @book = Book.find(params[:id])
- @user = @book_user
-  redirect_to(books_path) unless @user==current_user
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
