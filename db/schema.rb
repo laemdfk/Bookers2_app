3de52cef35125619615aa4f4d5ecb721c93e1d3b
EC2 Default User <ec2-user@ip-172-31-2-155.ap-northeast-1.compute.internal>
2022-04-08 14:07:07 +0000

    [Update]model名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_123811) do
+ActiveRecord::Schema.define(version: 2022_03_18_123811) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
