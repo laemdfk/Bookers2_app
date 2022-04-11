186fa6ebcb57a2cc022fc4d5acf41efee41381eb
EC2 Default User <ec2-user@ip-172-31-2-155.ap-northeast-1.compute.internal>
2022-04-11 16:19:13 +0000

    [Update]introductionのエラー解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_133029) do
+ActiveRecord::Schema.define(version: 2022_04_11_161709) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
